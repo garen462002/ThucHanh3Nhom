a1541294db816ba3489fe76805a069c6c3e52dbb
HELLO <HELLO@172.22.138.221>
2023-05-10 17:03:20 +0700

    Commited !!!

---
+++ Group3/src/thuchanh3/Thuchanh3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package thuchanh3;
+
+/**
+ *
+ * @author Tam Tue
+ */
+public class Thuchanh3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        tinhDiem a = new tinhDiem();
+        a.setVisible(true);
+    }
+    
+}
---
+++ Group3/src/thuchanh3/tinh.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thuchanh3;
+
+/**
+ *
+ * @author Tam Tue
+ */
+public class tinh {
+    double tinhD(double dcc,double dtk, double dgk, double dck){
+        return (dcc*10/100+dtk*15/100+dgk*20/100+dck*55/100);
+    } 
+}
